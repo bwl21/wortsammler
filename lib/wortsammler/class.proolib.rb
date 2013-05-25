67676573f01c87560853abe3bfb0be6ae9d1f15d
Bernhard Weichel <github.com@nospam.weichel21.de>
2013-05-25 18:54:55 +0200

    Fixed the default encodings

--- lib/wortsammler/class.proolib.rb
+++ lib/wortsammler/class.proolib.rb
@@ -18,7 +18,8 @@ require 'nokogiri'
 require "rubyXL"
 require 'logger'
 
-
+Encoding.default_external = Encoding::UTF_8
+Encoding.default_internal = Encoding::UTF_8
 
 # TODO: make these patterns part of the configuration
 
